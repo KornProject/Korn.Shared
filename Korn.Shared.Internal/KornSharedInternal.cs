e17328883a2345a1d2d999373263d7c7882743de
Yotic <yoticcitoy@gmail.com>
2025-03-01 10:13:59 +0300

    Removed internal module from this repository. Transfered paths from Korn.Shared to Korn.Interface

--- Korn.Shared.Internal/KornSharedInternal.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Korn.Shared.Internal
-{
-    public static class KornSharedInternal
-    {
-        public const string RootDirectory = @"C:\Program Files\Korn";
-    }
-}
--- Korn.Shared/KornShared.cs
+++ Korn.Shared/KornShared.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Korn.Shared.Internal;
+using Newtonsoft.Json;
+using System;
 
 namespace Korn.Shared
 {
     public static class KornShared
     {
+        public const string RootDirectory = KornSharedInternal.RootDirectory;
         public static readonly Random Random = new Random();
-        public static readonly KornLogger Logger = new KornLogger(KornPaths.SharedLogFile);
+        public static readonly KornLogger Logger = new KornLogger(Korn.Interface.SharedData.LogFile);
+        public static readonly JsonSerializerSettings JsonSettings = KornSharedInternal.JsonSettings;
 
         static KornShared()
         {
